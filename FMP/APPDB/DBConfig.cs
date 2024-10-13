13e229ae1cb092418dea8af9ac57d827e3f68d1d
M0hammed <mmm2016alkaf@gmail.com>
2024-10-14 01:45:22 +0300

    Add code to models and DBAPP to create Database(just make migration)

---
+++ FMP/APPDB/DBConfig.cs
@@ -0,0 +1,17 @@
+﻿using FMP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FMP.APPDB
+{
+    public class DBConfig:DbContext
+    {
+        public DbSet<Ticket> tickets { get; set; }
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Customer> customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=FMPDB;Integrated Security=True");
+        }
+    }
+}
--- FMP/Models/Movie.cs
+++ FMP/Models/Movie.cs
@@ -1,6 +1,23 @@
-﻿namespace FMP.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FMP.Models
 {
     public class Movie
     {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [DisplayFormat(DataFormatString = "{0:HH:mm:ss}")]
+        public string? Movie_Duration { get; set; }
+        public string? Description { get; set; }
+        public virtual ICollection<Ticket> Tickets { get;}
     }
 }
