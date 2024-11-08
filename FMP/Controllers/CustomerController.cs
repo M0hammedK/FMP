533f2b5b3d7b255688ccf1f0793b375ee92bd2c8
M0hammed <mmm2016alkaf@gmail.com>
2024-11-08 20:31:15 +0300

    finished

--- FMP/Controllers/CustomerController.cs
+++ FMP/Controllers/CustomerController.cs
@@ -104,7 +104,113 @@ namespace FMP.Controllers
             }
             catch (Exception ex) { return StatusCode(500, ex); }
         }
+        [HttpPut("EditCustomer/{id}")]
+        public IActionResult EditC(int id, [FromBody] NewCustomer customer)
+        {
+            try
+            {
+                Customer c = new Customer();
+                if (id != 0)
+                    if (customer.Password == customer.Password2)
+                    {
+                        c.Id = id;
+                        c.Name = customer.Name;
+                        c.Email = customer.Email;
+                        c.PhoneNumber = customer.PhoneNumber;
+                        c.Password = customer.Password;
+                        c.Age = customer.Age;
+                        _context.Customers.Update(c);
+                        _context.SaveChanges();
+                    }
+                return Ok(c);
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
 
+        }
 
+        [HttpPut("EditMovie/{id}")]
+        public IActionResult EditM(int id, [FromBody] NewMovie movie)
+        {
+            try
+            {
+                Movie m = new Movie();
+                if(id != 0)
+                {
+                    m.Id = id;
+                    m.Title = movie.Title;
+                    m.Price = movie.Price;
+                    m.MovieDuration = movie.MovieDuration;
+                    m.Description = movie.Description;
+                    m.Date = movie.Date;
+                    _context.Movies.Update(m);
+                    _context.SaveChanges();
+                }
+                return Ok(m);
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
+        }
+
+        [HttpPut("EditTicket/{id}")]
+        public IActionResult EditT(int id, [FromBody] NewTicket ticket)
+        {
+            Ticket t = new Ticket();
+            try
+            {
+                t.Id = id;
+                t.Date = ticket.Date;
+                t.CustomerId = ticket.CustomerId;
+                t.MovieId = ticket.MovieId;
+                t.Price = ticket.Price;
+                _context.Tickets.Update(t);
+                _context.SaveChanges();
+                return Ok(t);
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
+        }
+        [HttpDelete("DeleteCustomer/{id}")]
+        public IActionResult DeleteC(int id)
+        {
+            try
+            {
+                if (id != 0)
+                { 
+                    _context.Customers.Remove(_context.Customers.FirstOrDefault(x => x.Id == id));
+                    _context.SaveChanges();
+                }
+                return NoContent();
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
+
+        }
+
+        [HttpDelete("DeleteMovie/{id}")]
+        public IActionResult DeleteM(int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    _context.Movies.Remove(_context.Movies.FirstOrDefault(x => x.Id == id));
+                    _context.SaveChanges();
+                }
+                return NoContent();
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
+        }
+
+        [HttpDelete("DeleteTicket/{id}")]
+        public IActionResult DeleteT(int id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    _context.Tickets.Remove(_context.Tickets.FirstOrDefault(x => x.Id == id));
+                    _context.SaveChanges();
+                }
+                return NoContent();
+            }
+            catch (Exception ex) { return StatusCode(500, ex); }
+        }
     }
 }
